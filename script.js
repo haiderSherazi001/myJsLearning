5b827020a069af8a1d3ea388b562e3fc3b0cde17
sherix <mrhyder29@gmail.com>
2025-11-26 15:27:33 +0500

    some practice

--- script.js
+++ script.js
@@ -1,1 +1,97 @@
-let a = 7;
+//variables
+
+let name = "Haider";  //string
+// let age = 22;   //number
+// let bignum = BigInt(12345678384388743479765433n);
+// let cgpa = 3.14;   //decimal number
+// let married = false;  //bool
+// let employeed = null; //null
+// let undef; //undefined
+// const person = {
+//     name:"sherix",
+//     age:22,
+//     height:5.9,
+//     cgpa:3.4,
+//     color:"brown",
+//     intro(){
+//         console.log("Hello there. My name is " + this.name);
+//     }
+// };  //object named person
+// console.log(name);
+// console.log(age);
+// console.log(cgpa);
+// console.log(married);
+// console.log(employeed);
+// console.log(bignum);
+// console.log(undef);
+// console.log(typeof(cgpa));  //typeof() function tells the type
+// console.log(typeof(age));
+// console.log(typeof(bignum));
+// document.write("hello");    //prints at the end of page
+
+//OBJECT
+// console.log(person.name);
+// person.intro();
+
+//if else
+
+// let grade = '';
+// let marks = 80;
+
+// if(marks>=80){
+//     grade = 'A';
+// }
+// else if(marks>=60){
+//     grade = 'B';
+// }
+// else if(marks >=40){
+//     grade = 'C';
+// }
+// else if(marks >= 20){
+//     grade = 'D';
+// }
+// else {
+//     grade = 'F';
+// }
+
+// console.log("Your grade is " + grade + '.');
+
+//switch
+
+// switch (grade) {
+//   case "A":
+//     console.log("It's an A.");
+//     break;
+//   case "B":
+//     console.log("It's a B.");
+//     break;
+//   case "C":
+//     console.log("It's a C.");
+//     break;
+//   case "D":
+//     console.log("It's a D.");
+//     break;
+//   default:
+//     console.log("It's an F.");
+//     break;
+// }
+
+//for loop
+
+// for (let i = 0; i <= 100; i++){
+//     console.log(i);
+// }
+
+let text = `Hello. My name is ${name}.`;
+// console.log(text);
+
+console.log(text.length);
+console.log(text.charAt(0)); //gives character at given position
+console.log(text.charAt(1));
+console.log(text.charAt(2));
+console.log(text.charAt(3));
+console.log(text.charAt(4));
+console.log(text.charCodeAt(4));    //gives character code ascii at given position
+console.log(text[0]);
+
+
